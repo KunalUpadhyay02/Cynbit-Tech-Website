a1a80fee6dda2c27c36555d9dfdc2147e268e252
KunalUpadhyay02 <kunal2upadhyay@gmail.com>
2024-05-16 16:56:27 +0530

    "Commit on 16-05-2024"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Testimonials from './Components/Testimonials/Testimonials'
-import Title from './Components/Title/Title'
-import Contacts from './Components/Contact/Contacts'
+import React, { useState } from 'react';
+import Modal from 'react-modal'; 
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Title from './Components/Title/Title';
+import Contacts from './Components/Contact/Contacts';
+import Footer from './Components/Footer/Footers';
+import './index.css'
+
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div>
       <Navbar/>
       <Hero/>
       <div className="container">
-        <Title subTitle='Testimonials' title = 'What our client says'/>
+        <Title subTitle='Testimonials' title='What our client says'/>
         <Testimonials/>
-        <Title subTitle='Contact Us' title = 'Get in Touch'/>
-        <Contacts/>
+        <div className="button-container">
+          <button className='btn-main' onClick={() => setVisible(true) }>Contact Us</button>
+          <Modal isOpen={visible} onRequestClose={() => setVisible(false)}>
+            <Title subTitle='Contact Us' title='Get in Touch'/>
+            <Contacts/>
+            <center>
+            <button className='btn-main' onClick={() => setVisible(false)}>Close Contact Form</button>
+            </center>
+          </Modal>
+        </div>
       </div>
+      <Footer/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Footer/Footers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import'./Footers.css'
+
+function Footer() {
+  return (
+    <div className='footer'>©2024. All rights reserved.</div>
+  )
+}
+
+export default Footer
